ad621b33fa059ad907994027c2499baab4730f83
Ian Baum <ibaum@gitlab.com>
2019-03-06 18:32:17 +0000

    Updates per MR
    * Use cross platform, symlink following method for finding script
    directory
    * Use functions for script actions
    * Find tools in common.sh
    * Don't auto-install eksctl

--- scripts/common.sh
+++ scripts/common.sh
@@ -2,6 +2,29 @@
 
 # Checks that appropriate gke params are set and
 # that gcloud and kubectl are properly installed and authenticated
+function need_tool(){
+  local tool="${1}"
+  local url="${2}"
+
+  echo >&2 "${tool} is required. Please follow ${url}"
+  exit 1
+}
+
+function need_gcloud(){
+  need_tool "gcloud" "https://cloud.google.com/sdk/downloads"
+}
+
+function need_kubectl(){
+  need_tool "kubectl" "https://kubernetes.io/docs/tasks/tools/install-kubectl"
+}
+
+function need_helm(){
+  need_tool "helm" "https://github.com/kubernetes/helm/blob/master/docs/install.md"
+}
+
+function need_eksctl(){
+  need_tool "eksctl" "https://exkctl.io"
+}
 
 function validate_required_tools(){
   if [ -z "$PROJECT" ]; then
@@ -9,9 +32,10 @@ function validate_required_tools(){
     exit 1;
   fi
 
-  command -v gcloud  >/dev/null 2>&1 || { echo >&2 "gcloud is required please follow: https://cloud.google.com/sdk/downloads"; exit 1; }
-  command -v kubectl >/dev/null 2>&1 || { echo >&2 "kubectl is required please follow: https://kubernetes.io/docs/tasks/tools/install-kubectl"; exit 1; }
-  command -v helm    >/dev/null 2>&1 || { echo >&2 "helm is required please follow: https://github.com/kubernetes/helm/blob/master/docs/install.md"; exit 1; }
+  for comm in gcloud kubectl helm
+  do
+    command  -v "${comm}" > /dev/null 2>&1 || "need_${comm}"
+  done
 
   gcloud container clusters list >/dev/null 2>&1 || { echo >&2 "Gcloud seems to be configured incorrectly or authentication is unsuccessfull"; exit 1; }
 
@@ -19,4 +43,11 @@ function validate_required_tools(){
 
 function cluster_admin_password_gke(){
   gcloud container clusters describe $CLUSTER_NAME --zone $ZONE --project $PROJECT --format='value(masterAuth.password)';
+}
+
+function validate_eks_required_tools(){
+  for comm in eksctl kubectl helm
+  do
+    command -v "${comm}" > /dev/null 2>&1 || "need_${comm}"
+  done
 }
