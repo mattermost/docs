19ed1d98b83c4c76c3b5f7c959ad7d1f9204a541
DJ Mountney <dj@gitlab.com>
2019-03-25 16:23:01 +0000

    Merge branch 'dev-version-file-location' into 'master'
    Use API to fetch VERSION files from dev
    
    See merge request charts/gitlab!726

--- scripts/lib/version_fetcher.rb
+++ scripts/lib/version_fetcher.rb
@@ -2,19 +2,29 @@ require_relative 'version'
 
 require 'open-uri'
 require 'uri'
+require 'cgi'
 
 class VersionFetcher
   def initialize(version, repo)
     @version = Version.new(version)
     @repo = repo
+    @api_token = ENV['FETCH_DEV_ARTIFACTS_PAT']
+    @api_url = if @repo.start_with?('gitlab/')
+                 'https://dev.gitlab.org/api/v4'
+               elsif @repo.start_with?('gitlab-org/')
+                 'https://gitlab.com/api/v4'
+               else
+                 ENV['CI_API_V4_URL']
+               end
   end
 
   # GitLab Shell Version
   def gitlab_shell
     return @version if @version == 'master'
 
-    url = "#{@repo}/raw/#{ref(@version)}/GITLAB_SHELL_VERSION"
-    new_version = URI.parse(url).read.strip
+    url = "#{@api_url}/projects/#{CGI.escape(@repo)}/repository/files/GITLAB_SHELL_VERSION/raw?ref=#{ref(@version)}"
+    $stdout.puts "Getting GitLab Shell version from #{url}"
+    new_version = open(url, 'PRIVATE-TOKEN' => @api_token).read.strip
     $stdout.puts "# Shell appVersion: #{new_version}"
     new_version
   end
@@ -23,8 +33,9 @@ class VersionFetcher
   def gitaly
     return @version if @version == 'master'
 
-    url = "#{@repo}/raw/#{ref(@version)}/GITALY_SERVER_VERSION"
-    new_version = URI.parse(url).read.strip
+    url = "#{@api_url}/projects/#{CGI.escape(@repo)}/repository/files/GITALY_SERVER_VERSION/raw?ref=#{ref(@version)}"
+    $stdout.puts "Getting Gitaly version from #{url}"
+    new_version = open(url, 'PRIVATE-TOKEN' => @api_token).read.strip
     $stdout.puts "# Gitaly appVersion: #{new_version}"
     new_version
   end
