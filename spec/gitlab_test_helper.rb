8c8e8f74c1ea1742695e13591ee3d6fb78bd01b0
DJ Mountney <david@twkie.net>
2019-06-19 17:52:24 -0700

    Test with a newer backup, from 11.11.3
    Migrating from versions older than 11.11 is not supported by GitLab

--- spec/gitlab_test_helper.rb
+++ spec/gitlab_test_helper.rb
@@ -83,7 +83,7 @@ module Gitlab
     end
 
     def restore_from_backup
-      backup = ENV['BACKUP_TIMESTAMP'] || '0_11.6.0-pre'
+      backup = ENV['BACKUP_TIMESTAMP'] || '0_11.11.3'
       cmd = full_command("backup-utility --restore -t #{backup}")
       stdout, status = Open3.capture2e(cmd)
 
@@ -159,7 +159,7 @@ module Gitlab
 
     def ensure_backups_on_object_storage
       storage_url = 'https://storage.googleapis.com/gitlab-charts-ci/test-backups'
-      backup_file_names = ['11.6.0-pre_gitlab_backup.tar']
+      backup_file_names = ['11.11.3_gitlab_backup.tar']
       backup_file_names.each do |file_name|
         file = open("#{storage_url}/#{file_name}").read
         object_storage.put_object(
