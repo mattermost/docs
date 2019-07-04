dbd40f05b79139c7f040be6f177e9cb973ef59a6
DJ Mountney <david@twkie.net>
2019-07-04 08:10:51 -0700

    Add 2 retries to our spec tests
    This matches how GitLab QA is currently running, and should hopefully
    reduce failures caused by unicorn restarts.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -47,4 +47,13 @@ RSpec.configure do |config|
   config.define_derived_metadata(file_path: %r{/spec/features/}) do |metadata|
     metadata[:type] = :feature
   end
+
+  # show retry status in spec process
+  # show exception that triggers a retry if verbose_retry is set to true
+  config.verbose_retry = true
+  config.display_try_failure_messages = true
+
+  config.around do |example|
+    example.run_with_retry retry: 2
+  end
 end
