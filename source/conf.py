afd9ce417ea312d81710af00fd844f8447011f05
Sandro Spadaro <sandro.spadaro@gmail.com>
2020-03-14 17:19:56 +0100

    setup project for translation

--- source/conf.py
+++ source/conf.py
@@ -319,3 +319,6 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
+
+locale_dirs = ['locale/']   # path is example but recommended.
+gettext_compact = False     # optional.
