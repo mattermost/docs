933a67cb880753bfa5bc2eac1bbdb633527da84b
Asaad Mahmood <asaadmahmood@users.noreply.github.com>
2020-03-20 16:06:38 +0500

    Adding customer thermometer for pages

--- source/myscript.js
+++ source/myscript.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
+	const customerFeedback = sessionStorage.getItem("customerFeedback");
+
+	if (!customerFeedback) {
+		$('body').append("<div class='c-thermometer'><p class='c-thermometer__paragraph'>Is this page helpful?</p> <div class='c-thermometer__emojis'> <a href='javascript:void(0)' onClick='ga('send', 'event', { eventCategory: 'Feedback', eventAction: 'Click', eventLabel: 'Excellent', eventValue: 1});'> <span class='c-thermometer__emoji'>😀</span> <p>Excellent</p> </a> <a href='javascript:void(0)' onClick='ga('send', 'event', { eventCategory: 'Feedback', eventAction: 'Click', eventLabel: 'Good', eventValue: 2});'> <span class='c-thermometer__emoji'>🙂</span> <p>Good</p> </a> <a href='javascript:void(0)' onClick='ga('send', 'event', { eventCategory: 'Feedback', eventAction: 'Click', eventLabel: 'Average', eventValue: 3});'> <span class='c-thermometer__emoji'>😐</span> <p>Average</p> </a> <a href='javascript:void(0)' onClick='ga('send', 'event', { eventCategory: 'Feedback', eventAction: 'Click', eventLabel: 'Poor', eventValue: 4});'> <span class='c-thermometer__emoji'>🙁</span> <p>Poor</p> </a> </div> </div>");
+	}
 
 	$('header .links__icon').on('click', function (){
 		$("header .header__searchbar").hide();
@@ -18,5 +23,17 @@ $(document).ready(function(){
 		var sidebarScrollPosition = $('.wy-menu-vertical li.current>a').offset().top;
 		$('.wy-side-scroll').scrollTop(sidebarScrollPosition-120);
 	}
-	
+
+	$('body').on('click', '.c-thermometer__emojis a', function(){
+		$(this).parent().hide();
+		sessionStorage.setItem("customerFeedback", "Done");
+    });
+
+    $('body').on('click', '.c-thermometer__emojis a', function(){
+		$('.c-thermometer__paragraph').text('Thank you for submitting your rating.');
+		setTimeout(() => {
+			$('.c-thermometer').fadeOut();
+		}, 3000);
+	});
+
 });
