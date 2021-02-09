5e91331e2bab4f97055fe786c271ba46fce898e2
Justine Geffen <justinegeffen@users.noreply.github.com>
2021-02-09 22:18:39 +0200

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -55,16 +55,12 @@ templates_path = ['templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
-source_suffix = {
-    '.rst': 'restructuredtext',
-    '.txt': 'markdown',
-    '.md': 'markdown',
-}
-
+source_suffix = ['.rst', '.md']
 # source_suffix = '.rst'
 
 source_parsers = {
     '.md': CommonMarkParser,
+}
 
 # The encoding of source files.
 #source_encoding = 'utf-8-sig'
