4871866f2485ac477356d227fb6732c8006cbaaa
Justine Geffen <justinegeffen@users.noreply.github.com>
2021-04-15 16:09:22 +0200

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -95,7 +95,7 @@ redirects = {
      "process/bug-severity-guidelines": "https://handbook.mattermost.com/operations/research-and-development/product/development-process/new-bug-tickets/bug-severity-guidelines",
      "process/accepting-pull-request": "https://handbook.mattermost.com/contributors/contributors/help-wanted",
      "process/pm-faq": "https://handbook.mattermost.com/operations/research-and-development/product/product-management-team-handbook#frequently-asked-questions-faq",
-     
+    
 }
 
 # The master toctree document.
