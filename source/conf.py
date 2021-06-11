e68fd03a46238ec2ca7fa178b44dd09ace8e3953
Justine Geffen <justine.geffen@mattermost.com>
2021-06-11 17:01:42 +0200

    Updated HTML theme options.

--- source/conf.py
+++ source/conf.py
@@ -190,7 +190,7 @@ html_theme = 'sphinx_rtd_theme'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {
+html_theme_options = {
     'collapse_navigation': True,
     'sticky_navigation': True,
     'navigation_depth': 4,
