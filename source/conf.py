6496d7cc78055046b6a177f6a889a2e213a0fd74
Justine Geffen <justine.geffen@mattermost.com>
2021-06-11 17:06:23 +0200

    Adjusted HTML settings.

--- source/conf.py
+++ source/conf.py
@@ -191,11 +191,11 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
-    'collapse_navigation': True,
+    'collapse_navigation': False,
     'sticky_navigation': True,
     'navigation_depth': 4,
     'includehidden': True,
-    'titles_only': True
+    'titles_only': False
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
