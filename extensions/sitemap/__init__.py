a63395bc555fa3cd821ce448afd714a86501b30f
Pankaj Bhattarai <68217624+im-endangered@users.noreply.github.com>
2021-10-08 18:48:19 +0530

    Update __init__.py
    Changed the comment method for easy readability

--- extensions/sitemap/__init__.py
+++ extensions/sitemap/__init__.py
@@ -1,21 +1,21 @@
-#
-# sitemap - The sphinx_sitemap extension with parallel read and write support enabled
-#
-# Parallel read/write support by Alan Lew <alan@ethereal.cc> (https://github.com/neflyte/)
-#
-# Copyright (c) 2013 Michael Dowling <mtdowling@gmail.com>
-# Copyright (c) 2017 Jared Dillard <jared.dillard@gmail.com>
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
+'''
+sitemap - The sphinx_sitemap extension with parallel read and write support enabled
 
+Parallel read/write support by Alan Lew <alan@ethereal.cc> (https://github.com/neflyte/)
+
+Copyright (c) 2013 Michael Dowling <mtdowling@gmail.com>
+Copyright (c) 2017 Jared Dillard <jared.dillard@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+'''
 import os
 import sphinx.builders
 from typing import Dict, List, Optional, Tuple, Any
