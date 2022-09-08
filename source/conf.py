71ea71982ac30f28366d3624a4b176008c23c461
Justine Geffen <justinegeffen@users.noreply.github.com>
2022-09-08 18:36:56 +0200

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -78,6 +78,10 @@ redirects = {
         "https://docs.mattermost.com/about/subscription.html#do-you-have-a-program-for-official-non-profits-open-source-projects-and-charities",
 "about/deployments-and-editions.html":
         "https://docs.mattermost.com/about/editions-and-offerings.html",
+"https://docs.mattermost.com/about/faq-business.html#copyright-and-licensing-of-mattermost-server-open-source-code":
+	"http://docs.mattermost.com/about/faq-license.html#open-source-derivative-work-of-mattermost",
+"https://docs.mattermost.com/about/faq-enterprise.html#what-are-mattermost-s-policies-around-licensing-terms-of-use-and-privacy":
+	"http://docs.mattermost.com/about/faq-license.html#what-are-mattermost-s-policies-around-licensing-terms-of-use-and-privacy",
 
 # Administration redirects
 "administration/announcement-banner.html":
