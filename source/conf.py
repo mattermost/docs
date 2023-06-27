01102597f32f373b0526227b2be5c7d4a0eb6caa
Carrie Warner (Mattermost) <74422101+cwarnermm@users.noreply.github.com>
2023-06-27 08:30:59 -0400

    Added missing redirect

--- source/conf.py
+++ source/conf.py
@@ -108,6 +108,8 @@ redirects = {
         "https://docs.mattermost.com/install/self-managed-changelog.html#id41",
 "administration/command-line-tools.html":
         "https://docs.mattermost.com/manage/command-line-tools.html",
+"administration/command-line-tools.html#mattermost-user-delete":
+	"https://docs.mattermost.com/manage/command-line-tools.html#mattermost-user-delete",
 "administration/command-line-tools.html#mattermost-channel-restore":
         "https://docs.mattermost.com/manage/command-line-tools.html#mattermost-channel-restore",
 "administration/command-line-tools.html#mattermost-permissions-reset":
