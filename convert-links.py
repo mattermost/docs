c3aa84d41456c1b5f094341b9a3d55c21e405939
alan lew <alan@ethereal.cc>
2024-03-10 17:11:35 -0400

    More robust ignoring of directories; Resolve redirects fully before constructing a directive

--- convert-links.py
+++ convert-links.py
@@ -4,6 +4,24 @@ from pathlib import Path
 from source.conf import redirects
 
 LINK_PATTERN = r"([^>`]?)`([^<`]*)<([^>@ ]+)>`_[_]?"
+IGNORE_DIRECTORIES = [
+    "_static",
+    "archive",
+    "images",
+    "recipes",
+    "repeatable-processes",
+    "samples",
+    "scripts",
+]
+
+
+def resolve_redirect(filename: str) -> str:
+    if filename in redirects:
+        target = redirects[filename]
+        if target.startswith("https://docs.mattermost.com/"):
+            target = target.removeprefix("https://docs.mattermost.com/")
+            return resolve_redirect(target)
+    return filename
 
 
 def process_match(match: re.Match) -> str:
@@ -23,11 +41,16 @@ def process_match(match: re.Match) -> str:
     ):
         # print(f"SKIP -> {match.group(0)}")
         return match.group(0)
-    # if the link is the source of a page redirect, skip it
-    if link_url.removeprefix("/").removesuffix("/") in redirects:
-        # print(f"REDIRECT -> {match.group(0)}")
-        return match.group(0)
-    # print(f"match_prefix={match_prefix}, link_url={link_url}, link_description={link_description}")
+    # If the link is the source of a page redirect, recursively resolve the redirect
+    # until an end page is reached; use that end page as the link_url
+    link_url_stripped = link_url.removeprefix("/").removesuffix("/")
+    if link_url_stripped in redirects:
+        link_url_redirect = resolve_redirect(link_url_stripped)
+        link_url = f"/{link_url_redirect}"
+        # print(f"link_url -> {link_url}")
+    # print(
+    #     f"match_prefix={match_prefix}, link_url={link_url}, link_description={link_description}"
+    # )
     # if the link has a fragment, return a `ref` directive
     if "#" in link_url:
         toks = link_url.split("#", 1)
@@ -41,26 +64,33 @@ def process_match(match: re.Match) -> str:
     return f"{match_prefix}:doc:`{link_description}<{link_url}>`"
 
 
+def should_ignore_file(filename: str) -> bool:
+    for ignore_dir in IGNORE_DIRECTORIES:
+        if filename.startswith(f"source/{ignore_dir}"):
+            return True
+    return False
+
+
 if __name__ == "__main__":
-    # grep command: grep -aEon '[^>`]?`[^<`]*<[^>@ ]+>`_[_]?' source/comply/electronic-discovery.rst
-    # rst_file = Path("source/comply/electronic-discovery.rst")
-    # subbed = re.sub(LINK_PATTERN, process_match, rst_file.read_text("utf-8"))
-    # print(subbed)
+    # grep command: grep -aEon '[^>`]?`[^<`]*<[^>@ ]+>`_[_]?' xxxxx.rst
+    # rst_file = Path("source/onboard/shared-channels.rst")
+    # raw_content = rst_file.read_text("utf-8")
+    # re.sub(LINK_PATTERN, process_match, raw_content)
     source_path = Path("source")
     rst_files = sorted(source_path.glob("**/*.rst"))
     print(f"Process {len(rst_files)} rST files")
     for rst_file in rst_files:
-        # Skip any files in a /_static/ directory and skip any 'version-archive.rst' files
-        if "/_static/" in str(rst_file) or rst_file.name == "version-archive.rst":
+        rst_file_name = str(rst_file)
+        if (
+            should_ignore_file(rst_file_name)
+            or rst_file.name == "version-archive.rst"
+            or "changelog" in rst_file.name
+        ):
             print(f"-  {rst_file}")
             continue
         # Read the rST file
-        raw_content = rst_file.read_text("utf-8")
-        # If we read an orphan document, skip to the next file
-        if raw_content.startswith(":orphan:"):
-            print(f"#  {rst_file}")
-            continue
         print(f"o  {rst_file}")
+        raw_content = rst_file.read_text("utf-8")
         # Process rST links in the file
         subbed_content = re.sub(LINK_PATTERN, process_match, raw_content)
         # Write the processed file back to disk
