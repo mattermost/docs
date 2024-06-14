977bc4226878a42fed95cb3a25d35a233c96394f
Lorenzo Gallegos <1328683+enzowritescode@users.noreply.github.com>
2024-06-14 12:37:12 -0600

    Update .gitignore to ignore key and crt, fix gencert.sh and update gencert.renamed

--- source/scripts/generate-certificates/gencert.sh
+++ source/scripts/generate-certificates/gencert.sh
@@ -2,21 +2,45 @@
 
 umask 007
 
-certname="${CRT_FILENAME:-"mattermost-x509"}"
+FILE_NAME="${CRT_FILENAME:-"mattermost-x509"}"
+CERT="${FILE_NAME}.crt"
+KEY="${FILE_NAME}.key"
+CSR="${FILE_NAME}.csr"
+
+# generate key
+openssl genrsa -out $KEY 4096
+
+if [ $? -ne 0 ]; then
+    echo "Error generating key"
+    exit
+fi
+
+# generate certificate signing request
+openssl req \
+    -new \
+    -key $KEY \
+    -out $CSR \
+    -subj "/C=${CRT_C:-"US"}/L=${CRT_L:-"Palo Alto"}/O=${CRT_O:-"Mattermost"}/OU=${CRT_OU:-"DevOps"}/CN=${CRT_CN:-"base.example.com"}"
+
+if [ $? -ne 0 ]; then
+    echo "Error generating certificate signing request (csr)"
+    exit
+fi
+
+# generate self-signed certificate
 openssl x509 \
-    -in <(
-        openssl req \
-            -days 3650 \
-            -newkey rsa:4096 \
-            -nodes \
-            -keyout "${certname}.key" \
-            -subj "/C=${CRT_C:-"US"}/L=${CRT_L:-"Palo Alto"}/O=${CRT_O:-"Mattermost"}/OU=${CRT_OU:-"DevOps"}/CN=${CRT_CN:-"base.example.com"}"
-        ) \
     -req \
-    -signkey "${certname}.key" \
-    -sha256 \
     -days 3650 \
-    -out "${certname}.crt" \
+    -in $CSR \
+    -signkey $KEY \
+    -sha256 \
+    -out $CERT \
     -extfile <(echo -e "basicConstraints=critical,CA:true,pathlen:0\nsubjectAltName=${CRT_SAN:-"DNS.1:logs.example.com,DNS.2:metrics.example.com,IP.1:192.168.0.1,IP.2:127.0.0.1"}")
 
-chmod 600 ${certname}.crt
+if [ $? -ne 0 ]; then
+    echo "Error generating self-signed certificate"
+    exit
+fi
+
+rm $CSR
+chmod 600 $CERT
