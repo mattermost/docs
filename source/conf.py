68f336993c3df7f33bc87101e15305fa43b389c5
Carrie Warner (Mattermost) <74422101+cwarnermm@users.noreply.github.com>
2024-09-27 11:53:27 -0400

    Removed unnecessary CSS & reformatted SQL codeblocks

--- source/conf.py
+++ source/conf.py
@@ -3632,8 +3632,7 @@ html_static_path = ["_static"]
 html_css_files = [
     "css/mattermost-global.css",
     "css/homepage-v1.css",
-    "css/compass-icons.css",
-    "css/tablefix.css"
+    "css/compass-icons.css"
 ]
 
 # A list of JavaScript filenames. The entry must be a filename string or a tuple containing the filename string and the
